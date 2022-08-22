646130883295ab8f59086ff9ae839a4d6ff5a6d2
Marcus Caiado <92039896+marcuscaiado@users.noreply.github.com>
2022-08-22 17:51:35 -0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,11 +13,11 @@
 	$(window).on ('load', function (){
         if ($(".wow").length) { 
             var wow = new WOW ({
-                boxClass:     'wow',      // Animated element css class (default is wow)
-                animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
-                mobile:       true,       // Trigger animations on mobile devices (default is true)
-                live:         true,       // Act on asynchronously loaded content (default is true)
+                boxClass:     'wow',      
+                animateClass: 'animated', 
+                offset:       20,         
+                mobile:       true,      
+                live:         true,       
             });
             wow.init();
         }
